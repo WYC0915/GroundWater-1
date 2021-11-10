5926b675a0abeaf7905bac7c13217a191f19844e
WYC0915 <junior055365@gmail.com>
2021-11-10 16:39:56 +0800

    Update main.py

--- DataPreprocess/竹塘/main.py
+++ DataPreprocess/竹塘/main.py
@@ -1,55 +1,17 @@
-# -*- coding: utf-8 -*-
-"""
-Spyder Editor
-
-This is a temporary script file.
-"""
-
-"""
-clear;
-clc;
-close all;
-
-A = importdata("竹塘(matlab2).xlsx");
-
-S_depth = A.data(2:end,1);
-T = A.data(1,2:end);
-V = A.data(2:end,2:end);
-T = string(T);
-T = datenum(T,'yyyymm');
-
-figure();
-hold on;
-
-colorbar= colorbar('YLim',[-1.5 2]);
-surf(T,S_depth,V,'edgecolor','none');
-h = colorbar;
-ylabel(h, 'Displacement(cm)','fontsize',16,'fontname','Times New Roman','LineWidth',1.5)
-set(gca,'YDir','reverse'); 
-xticks(T);
-%datetick('x','yyyy/mm');
-xticklabels(num2cell(string(datestr(T,'yyyy/mm'))));
-xlabel("Time (year/month)",'fontsize',14,'fontname','Times New Roman')
-ylabel("Depth(m)",'fontsize',14,'fontname','Times New Roman')
-colormap jet;
-axis tight;
-box on;
-shading interp;
-ylim([0 300]);
-xtickangle(270);
-set(gca,'FontName','Times New Roman','FontSize',14,'LineWidth',1.5);
-"""
-
-import numpy as np
 import time
+import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
 from matplotlib.colors import LightSource
-import matplotlib.colors as mcolors
-from mpl_toolkits.mplot3d import Axes3D
-from mpl_toolkits import mplot3d
 from matplotlib import cm
+import matplotlib.pyplot as plt #繪圖用的模組
+
+from mpl_toolkits import mplot3d
+from mpl_toolkits.mplot3d import Axes3D #繪製3D座標的函式
+#from mpl_toolkits.mplot3d import axes3d #繪製3D座標的函式
+import matplotlib.ticker as ticker # 處理figure間隔
+
 A = pd.read_excel("竹塘(matlab2).xlsx")
+
 S_depth = A.iloc[0:, 0]
 S_depth = S_depth.values.round(3).tolist()
 T = list(A)
@@ -58,76 +20,61 @@ for i, t in enumerate(T):
     t=str(t)
     t = time.strptime(t, "%Y%m")
     t = time.strftime("%Y/%m",t)
+
 V=A.iloc[:,1:]
 V=V.values.round(3)
 V=V.tolist()
-#try to trans to np
 V=np.array(V)
 T=np.array(T)
 S_depth=np.array(S_depth)
 
-
-#colorbar=plt.colorbar('YLim',[-1.5,2])
-
-
+print("")
 print(np.shape(V),np.shape(np.transpose(V)),V.dtype)
 print(np.shape(T),T.dtype)
 print(np.shape(S_depth),S_depth.dtype)
 print(np.shape([1,2,3]))
-##init
+print("")
+
+# 建立 3D 圖形
 fig = plt.figure()
-ax = fig.gca(projection='3d')
-#camera init
-ax.view_init(elev=45,azim=0)
-#create
+ax=Axes3D(fig)
+# ax = fig.gca(projection='3d')
+
+# 使figure間隔變多
+plt.xticks(np.arange(min(T), max(T)+10, 10.0))
+plt.yticks(np.arange(min(S_depth), max(S_depth)+10, 10.0))
+
+#ax.xaxis.set_major_locator(ticker.MultipleLocator(T))
+#ax.yaxis.set_major_locator(ticker.MultipleLocator(S_depth))
+
+
+# 用這兩個arange物件中的可能取值一一對映去擴充為所有可能的取樣點
 T,S_depth = np.meshgrid(T,S_depth)
 
-#cmap = plt.get_cmap('jet')
-cNorm = mcolors.Normalize(vmin=-2, vmax=1)
+# 繪製 3D 曲面圖形
+surf = ax.plot_surface(T, S_depth, V, rstride=1, cstride=1, cmap='jet')# viridis jet
+ax.view_init(elev=90,azim=0)
 
-surf = ax.plot_surface(T, S_depth, V ,cmap='jet')
-#plt.pcolormesh(T, S_depth, V, vmin=-2, vmax=1, cmap="RdBu_r")
-#plt.colorbar(ax=None)
+plt.xticks(rotation=320)
+plt.yticks(rotation=0)
 
-plt.draw()
+# 繪製 Colorbar 圖形
+fig.colorbar(surf, ax=ax, shrink=0.75, aspect=12)
 
+# label字樣...
+ax.set_xlabel('Time (year/month)', fontsize = 14, fontfamily = 'Times New Roman', color = 'black')# sans-serif
+ax.set_ylabel('Depth(m)'         , fontsize = 14, fontfamily = 'Times New Roman', color = 'black')
+ax.set_zlabel('Displacement(cm)' , fontsize = 16, fontfamily = 'Times New Roman', color = 'black')
 
+cmap = plt.cm.copper
+ls = LightSource(315, 45)
+rgb = ls.shade(V, cmap)
+#ax.imshow(rgb, interpolation='bilinear')
+#im = ax.imshow(V, cmap=cmap)
+#im.remove()
+#fig.colorbar(im)
 
-###bar
-#fig.colorbar(surf, ax=ax, shrink=0.5)#, aspect=5
+plt.box(on=True)
 
-#mycmap = plt.get_cmap('gist_earth')
-#ax.set_title('gist_earth color map')
-#surf = ax.plot_surface(X, Y, Z, cmap=mycmap)
-#plt.box(on=True)
-#ls = LightSource(azdeg=315, altdeg=45)
-#sha = ls.shade(V,cmap='viridis', blend_mode='overlay')
-#sha = ls.shade(V,cmap=plt.cm.copper, blend_mode='overlay')
-#surf = plt.surf(T,S_depth,V,'edgecolor','none')
-#h = plt.colorbar(surf)
-#plt.ylim(-0.5,2.5)
-
-#h = colorbar
-#plt.ylabel(h, 'Displacement(cm)','fontsize',16,'fontname','Times New Roman','LineWidth',1.5)
-#set(gca,'YDir','reverse'); 
-#plt.gca().set(xlim=(0.0, 0.1), ylim=(-0.5,2.5),
- #             xlabel=("Time (year/month)",'fontsize',14,'fontname','Times New Roman'), ylabel='reverse')
-#plt.set(plt.gca,'YDir','reverse')
-#plt.xticks(T)
-
-#plt.xlabel(plt.num2cell(str(time.strftime('yyyy/mm',T))))
-#plt.xlabel(T)
-
-#
-#ax.pcolormesh(T, S_depth, V[:-1, :-1], shading='interp', vmin=V.min(), vmax=V.max())
-#plt.draw()
-
-#plt.xticklabels(num2cell(str(time.strftime('yyyy/mm',T))));
-#%datetick('x','yyyy/mm')
-
-#S_depth=
-#T=A.head
-#print(S_depth)
-#print(T)
-#print(V)
-#print(np.array(V).tolist())
+# 顯示所有 figure 物件
+plt.show()
